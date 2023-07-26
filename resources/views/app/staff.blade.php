e11b87e3b0e40e1d81431dba50c07c32b2aa4689
Martin Njoroge <martinnjoroge0028@gmail.com>
2023-07-27 00:41:54 +0300

    Addedd staff page

---
+++ resources/views/app/staff.blade.php
@@ -0,0 +1,177 @@
+@extends('layouts.app')
+
+@section('content')
+        <div class="row mt-3">
+            <div class="col-12">
+            <button  style="background-color: #fac107;color: white" type="button" class="btn right" data-bs-toggle="modal" data-bs-target="#con-close-modal-add-1">
+                  <i class='fa fa-plus' aria-hidden='true'></i>   Add Settings
+                </button>
+                <div class="card" style="border-radius:0px 15px 15px 15px;box-shadow: 2px 3px 3px 2px rgba(255, 239, 9, 0.179);">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="datatable" class="table table-sm dt-responsive nowrap text-center">
+                                <thead class="table-light">
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Value</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+
+
+                                <tbody>
+                                    <tr>
+                                        <td>n</td>
+                                        <td>n</td>
+                                        <td style='font-size:10px; text-align: center;'>
+                                            <button type="button" style="background-color: #fac107;color: white" class="btn btn-xs" data-bs-toggle="modal" data-bs-target="#con-close-modal-edit-settings-">
+                                                <i class='fas fa-pen' aria-hidden='true'></i>
+                                                </button>
+                                            <button type="button" onclick="del(this)" value="" class="btn btn-danger btn-xs">
+                                                <i class='fa fa-trash' aria-hidden='true'></i>
+                                            </button>
+
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div> <!-- end row -->
+
+
+
+
+
+@endsection
+
+@section('scripts')
+    <script>
+    $(document).ready(function(){
+//Add settings Form
+$("#settiingsForm").on('submit',(e)=>{
+e.preventDefault();
+var btn=$("#addbtn");
+var loader=$("#addloader")
+btn.hide();
+loader.show();
+let data=$("#settiingsForm").serialize();
+$.ajax({
+    type: "POST",
+    url: "#",
+    data: data,
+    success: function (response) {
+
+                    toastr.options = {
+                        "closeButton": false,
+                        "debug": false,
+                        "newestOnTop": false,
+                        "progressBar": true,
+                        "positionClass": "toast-top-right",
+                        "preventDuplicates": false,
+                        "onclick": null,
+                        "showDuration": "300",
+                        "hideDuration": "1000",
+                        "timeOut": "5000",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                    }
+                    toastr["success"]("", "Settings Saved Succesfully.")
+        location.href='#'
+    },
+    error: function(res){
+        Swal.fire("Error!", "Try again later...", "error");
+    }
+});
+})
+
+// Edit settings Form
+$(".settiingsEditForm").on('submit', function(e) {
+  e.preventDefault();
+
+  const form = $(this);
+  var itemId = form.find('input[name="editsettingId"]').val();
+  var btn = $("#editbtn" + itemId);
+  var loader = $("#editloader" + itemId);
+  btn.hide();
+  loader.show();
+  let data = form.serialize();
+$.ajax({
+    type: 'PATCH',
+    url: '/#/' + itemId,
+    data: data,
+    success: function (response) { console.log(response)
+
+                    toastr.options = {
+                        "closeButton": false,
+                        "debug": false,
+                        "newestOnTop": false,
+                        "progressBar": true,
+                        "positionClass": "toast-top-right",
+                        "preventDuplicates": false,
+                        "onclick": null,
+                        "showDuration": "300",
+                        "hideDuration": "1000",
+                        "timeOut": "5000",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                    }
+                    toastr["success"]("", "Settings Update Succesfully.")
+        location.href='#'
+    },
+    error: function(res){ console.log(res)
+        Swal.fire("Error!", "Try again later...", "error");
+    }
+});
+})
+
+
+    })
+    </script>
+
+    <script>
+
+        //Deleting Settings
+        function del(e){
+        let id=e.value;
+        var type=0;//For knowing deletion operation is coming from settings
+
+        Swal.fire({
+            title: "Confirm deletion",
+            text: "You won't be able to revert this!",
+            type: "error",
+            showCancelButton: !0,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((t)=>{
+        if(t.value){
+                $.ajax({
+                    type: "DELETE",
+                    url: "settings/"+id,
+                    data:{
+                        _token:"{{csrf_token()}}", id,type
+                    },
+                    success: function (response) { console.log(response)
+
+                        Swal.fire("Deleted", "Successfully.", "success").then(()=>{
+                        location.href='#'})
+                    },
+                    error: function(res){console.log(res)
+                        Swal.fire("Error!", "Try again later...", "error");
+                    }
+                });
+            }
+            })
+        }
+    </script>
+@endsection
