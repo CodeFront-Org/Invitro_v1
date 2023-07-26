005fc5b9f9f43c7e792be7ef49865442b2e91784
Martin Njoroge <martinnjoroge0028@gmail.com>
2023-07-26 12:22:59 +0300

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+//*****************Routes for registering Roles, Permission and Admin on first creation of the app***//
+Route::get('/Roles_registration_url',[App\Http\Controllers\RolesRegistration::class,'index'])->name('Roles_Regestration_url');//Registering roles built in app
+
+Route::get('/admin_register_url',function(){//Register admin
+    $label='Admin Register';
+    return view('auth.new_admin_register',compact('label'));
+});
+
+Route::get('/registration', function () {//Register customer
+    return view('auth.register');
+});
+//*******************************end registration */
 
 Auth::routes();
 
