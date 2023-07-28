78ada4d58472e76986acbd0451f6f3315e1aa945
Martin Njoroge <martinnjoroge0028@gmail.com>
2023-07-28 23:22:41 +0300

    updated return modal

--- resources/views/app/stock.blade.php
+++ resources/views/app/stock.blade.php
@@ -279,55 +279,6 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                    <label for="field-11w" class="form-label">Quantity Type</label>
-                                    <select name="role" class="form-control form-select" id="field-11w" required>
-                                                <option value="0">Cartons</option>
-                                                <option value="1">Packets</option>
-                                                <option value="1">Single Items</option>
-                                        </select>
-                                </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                    <label for="field-11q" class="form-label">Order Type</label>
-                                    <select name="role" class="form-control form-select" id="field-11q" required>
-                                                <option value="0">New</option>
-                                                <option value="1">Return</option>
-                                        </select>
-                                </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="field-2n" class="form-label">Amount</label>
-                                        <input type="number" name="amount" class="form-control" id="field-2n" placeholder="amount" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="field-2l" class="form-label">Order Level</label>
-                                        <input type="number" name="o_level" class="form-control" id="field-2l" placeholder="order level" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="field-2n" class="form-label">Source</label>
-                                        <input type="text" name="source" class="form-control" id="field-2n" placeholder="source" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="field-2l" class="form-label">Expiry Date</label>
-                                        <input type="date" name="e_date" class="form-control" id="field-2l" placeholder="expiry date" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
                                 <div class="col-md-12">
                                     <div class="mb-3">
                                         <label for="field-2" class="form-label">Remarks</label>
