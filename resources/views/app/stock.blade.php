58c984e02cf53090ca98cd5f4dcb0ded431111d6
Martin Njoroge <martinnjoroge0028@gmail.com>
2023-08-01 10:52:56 +0300

    updated ui

--- resources/views/app/stock.blade.php
+++ resources/views/app/stock.blade.php
@@ -210,90 +210,68 @@
 
 
 
-            <!-- Edit Stock Modal -->
+            <!-- View Transaction of Stock Modal -->
 
             <div id="con-close-modal-txn-1" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-                <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-dialog modal-full-width">
                     <div class="modal-content">
                         <form id="settiingsForm" method="post">
                         @csrf
                         @method('PATCH')
                         <input type="hidden" name="type" value="0">
                         <div class="modal-header">
-                            <h4 class="modal-title">View Transactions</h4>
+                            <h4 class="modal-title">Panadol Stock Transactions</h4>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                    <label for="field-11w" class="form-label">Product Name</label>
-                                    <select name="name" class="form-control form-select" id="field-11w" required>
-                                                <option value="0">Panadol</option>
-                                        </select>
-                                </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="field-2l" class="form-label">Quantity</label>
-                                        <input type="text" value="" name="quantity" class="form-control" id="field-2l" placeholder="quantity" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="field-2n" class="form-label">Amount per item</label>
-                                        <input type="number" value="" name="amount" class="form-control" id="field-2n" placeholder="amount per item" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="field-2l" class="form-label">Order Level</label>
-                                        <input type="number" value="" name="o_level" class="form-control" id="field-2l" placeholder="order level" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="field-2n" class="form-label">Source</label>
-                                        <input type="text" value="" name="source" class="form-control" id="field-2n" placeholder="source" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="field-2l" class="form-label">Expiry Date</label>
-                                        <input type="date" value="" name="e_date" class="form-control" id="field-2l" placeholder="expiry date" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="mb-3">
-                                    <label for="field-11w" class="form-label">Quantity Type</label>
-                                    <select name="role" class="form-control form-select" id="field-11w" required>
-                                                <option value="0">Cartons</option>
-                                                <option value="1">Packets</option>
-                                                <option value="1">Single Items</option>
-                                        </select>
-                                </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="mb-3">
-                                        <label for="field-2" class="form-label">Remarks</label>
-                                        <textarea id="textarea" value="" class="form-control" required maxlength="300" rows="3" placeholder="Your Remarks"></textarea>
-                                    </div>
-                                </div>
-                            </div>
+                        <div class="table-responsive">
+                            <table  style="font-family: 'Times New Roman', Times, serif" class="table table-bordered nowrap text-center" id="datatable" class="table table-sm table-bordered dt-responsive nowrap text-center">
+                                <thead class="table-light">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Quantity</th>
+                                    <th>Batch No</th>
+                                    <th>Order Level</th>
+                                    <th>Source</th>
+                                    <th>Staff</th>
+                                    <th>Date In</th>
+                                    <th>Expiry</th>
+                                    <th>Remarks</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+
+
+                                <tbody>
+                                    <tr>
+                                        <td>1. </td>
+                                        <td>50</td>
+                                        <td>20</td>
+                                        <td>P001</td>
+                                        <td>KEMSA</td>
+                                        <td>Martin Njoroge</td>
+                                        <td>20 August 2023</td>
+                                        <td>20 Aug</td>
+                                        <td>Good</td>
+                                        <td style='font-size:10px; text-align: center;'>
+                                            <button type="button" style="background-color: #08228a9f;color: white" class="btn btn-xs" data-bs-toggle="modal" data-bs-target="#con-close-modal-edit-">
+                                                <i class='fas fa-pen' aria-hidden='true'></i>
+                                                </button>
+                                            <button type="button" onclick="del(this)" value="" class="btn btn-danger btn-xs">
+                                                <i class='fa fa-trash' aria-hidden='true'></i>
+                                            </button>
+
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                         </div>
                         <div class="modal-footer">
-                            <button class="btn rounded-pill p-1" id="editbtn" style="width: 100%; background-color: #08228a9f;color: white" type="submit">
+                            <button class="btn p-1" id="editbtn" style="background-color: #08228a9f;color: white" type="submit">
                                     Submit
                             </button>
-                            <button class="btn rounded-pill p-1" id="editloader" style="width: 100%; background-color: #08228a9f;color: white;display:none;" type="button">
+                            <button class="btn p-1" id="editloader" style="background-color: #08228a9f;color: white;display:none;" type="button">
                                     <span class="spinner-border spinner-border-sm me-1" role="status" aria-hidden="true"></span>
                                     Saving Data...
                             </button>
