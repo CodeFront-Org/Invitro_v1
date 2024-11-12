5ef7bfc4012175d329c488c7922569006d9a12c6
mac <martinnjoroge0028@gmail.com>
2024-11-12 12:07:38 +0300

    Pushed to Github

--- V2/routes/web.php
+++ V2/routes/web.php
@@ -17,32 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-//*****************Routes for registering Roles, Permission and Admin on first creation of the app***//
-Route::get('/Roles_registration_url',[App\Http\Controllers\Auth\RolesRegistration::class,'index'])->name('Roles_Regestration_url');//Registering roles built in app
-
-Route::get('/admin_register_url',function(){//Register admin
-    $label='Admin Register';
-    return view('auth.new_admin_register',compact('label'));
-});
-
-Route::get('/registration', function () {//Register customer
-    return view('auth.register');
-});
-//*******************************end registration */
-
-
-//*******************************Testing Mails */
-Route::get('email-test',function(){
- return view('emails.order_level_notice');
-});
-
-//*******************************end testing mails */
-
-//***************** Reset Routes ******************/
-Route::post('/reset', [App\Http\Controllers\Auth\ResetController::class,'send_link'])->name('reset');//to send reset link
-Route::get('/reset-password/{id}/{token}', [App\Http\Controllers\Auth\ResetController::class,'index'])->name('reset-password');//to load reset psw page
-Route::post('/reset-psw', [App\Http\Controllers\Auth\ResetController::class,'reset'])->name('reset-psw');//to send reset link
-//************* End reset routes *******************/
 
 Auth::routes();
 Route::post('/new_admin_reg', [App\Http\Controllers\Auth\NewAdminRegister::class,'register'])->name('new_admin_reg');
@@ -77,12 +51,11 @@ Route::get('/complete-order', [App\Http\Controllers\OrderController::class, 'com
 
 
 Route::resource('/approve',App\Http\Controllers\ApproveController::class);
-//Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'profileUpdate'])->name('profile');
 
 Route::resource('/profile',App\Http\Controllers\ProfileController::class);
 Route::Post('/pswUpdate',[App\Http\Controllers\ProfileController::class,'pswUpdate'])->name('pswUpdate');
-
 Route::get('/product-details',[App\Http\Controllers\SearchController::class,'product'])->name('/product-details');
+
 // Reports routes 
 Route::get('/with-batch',[App\Http\Controllers\ReportsController::class,'productsWithBatch'])->name('/with-batch');
 Route::get('/without-batch',[App\Http\Controllers\ReportsController::class,'productsWithoutBatch'])->name('/without-batch');
@@ -97,9 +70,3 @@ Route::get('/reorder-level',[App\Http\Controllers\ReportsController::class,'prod
 //  Route to test code before implementing
 Route::get('/code',[App\Http\Controllers\TestController::class,'code'])->name('/code');
 Route::get('/cron',[App\Http\Controllers\CronController::class,'cron'])->name('cron');
-
-
-//Route::get('/profile',[App\Http\Controllers\UserController::class,'profile'])->name('profile');
-//Route::POST('/profile-update',[App\Http\Controllers\UserController::class,'updateProfile'])->name('profile-update');
-//Route::POST('/password-update',[App\Http\Controllers\UserController::class,'password'])->name('password-update');
-
