4b45d88744dab448b1397c75e8a48c4c86c90dea
thekuuri <nganga.n.kuuri@gmail.com>
2025-05-13 09:21:19 +0300

    Remove commented-out PHP code for user role display in left navbar for cleaner code

--- resources/views/components/leftnavbar.blade.php
+++ resources/views/components/leftnavbar.blade.php
@@ -6,13 +6,13 @@
                 <div id="sidebar-menu">
 
                     <ul id="side-menu">
-@php
+{{-- @php
 //display user roles
     $user = auth()->user();
     $userRole = $user->roles();
    echo "Role: " . implode(', ', $userRole->pluck('name')->toArray());
     //dd($userRole);
-@endphp
+@endphp --}}
                         <li class="menu-title">Navigation</li>
 
 	
