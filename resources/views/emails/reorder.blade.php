771abf1f60f4e7d93d1b90a1b445a4da70a33114
thekuuri <nganga.n.kuuri@gmail.com>
2026-05-26 12:16:18 +0300

    remove dashboard button for reorder email

--- resources/views/emails/reorder.blade.php
+++ resources/views/emails/reorder.blade.php
@@ -36,19 +36,12 @@
                                     <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                                         <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
                                             <p>
-                                                We wanted to inform you that the order level for the below products has been reached.
+                                                Please be informed that the order level for the below products has been reached.
                                                 Please take necessary actions to manage the inventory.
                                             </p>
                                         </td>
                                     </tr>
-                                    <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                                        <td class="content-block" itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
-                                            valign="top">
-                                            <p>Click the button below to proceed to the portal</p>
-                                            <a href="{{$link}}" class="btn-primary" itemprop="url" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; color: #FFF; text-decoration: none; line-height: 2em; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize; background-color:  #001d91f5; margin: 0; border-color: #001d91f5; border-style: solid; border-width: 8px 16px;">
-                                    Go to Dashboard</a>
-                                        </td>
-                                    </tr>
+                             
                                     <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                                         <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
                                             
